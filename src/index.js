ec9bbbaa8e2ce01765332a027546fc762cb97c5b
Huy Mai <huy.mai@ericsson.com>
2021-05-02 07:49:48 +0300

    Fix error with version detection at initialization
    The current logic should be reversed.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ export default class VueSocketIO {
      * @param Vue
      */
     install(Vue){
-        if (version >= +Vue.version.split('.')[0]) {
+        if (version <= +Vue.version.split('.')[0]) {
             Vue.config.globalProperties.$socket = this.io;
             Vue.config.globalProperties.$vueSocketIo = this;
         } else {
